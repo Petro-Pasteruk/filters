023035ae3e09e624c08733f55a38dccd6313aab3
Petro Pasteruk <petropasteruk@gmail.com>
2021-11-23 18:16:07 +0200

    Release version 2.1

--- js/main.js
+++ js/main.js
@@ -1,34 +1,3 @@
-function app() {
-    const
-        filters = document.querySelectorAll(".filter"),
-        cards = document.querySelectorAll('.card');
-
-    filters.forEach(filter => {
-        let filterName = filter.dataset.filterName;
-
-        filter.querySelectorAll(".button").forEach(btn => {
-            btn.addEventListener("click", function () {
-                if (btn.dataset.filter === "all") {
-                    cards.forEach(card => {
-                        card.classList.remove('hide');
-                        card.classList.remove('anime');
-                    });
-                } else {
-                    cards.forEach(card => {
-                        card.classList.remove('hide');
-                        card.classList.remove('anime');
-                        if (card.dataset[filterName] === btn.dataset.filter) {
-                            card.classList.remove('hide');
-                            card.classList.remove('anime');
-                        } else {
-                            card.classList.add('anime');
-                            card.classList.add('hide');
-                        }
-                    });
-                }
-            });
-        });
-    });
-}
-
+function app(){const
+    filters=document.querySelectorAll(".filter"),cards=document.querySelectorAll('.card');filters.forEach(filter=>{let filterName=filter.dataset.filterName;filter.querySelectorAll(".button").forEach(btn=>{btn.addEventListener("click",function(){if(btn.dataset.filter==="all"){cards.forEach(card=>{card.classList.remove('hide');card.classList.remove('anime')})}else{cards.forEach(card=>{card.classList.remove('hide');card.classList.remove('anime');if(card.dataset[filterName]===btn.dataset.filter){card.classList.remove('hide');card.classList.remove('anime')}else{card.classList.add('anime');card.classList.add('hide')}})}})})})}
 app();
